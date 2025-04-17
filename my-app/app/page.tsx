be1c8d3a44859b59a8fccfa30fa435c727246b6a
karthik <karthikbhatt22@gmail.com>
2025-04-17 22:10:53 +0530

    added kinde auth

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
-
-export default function Home() {
+import { prisma } from "./utils/db";
+async function getData() {
+ const data= await prisma.BlogPost.findMany({
+  select:{
+    title:true,
+    content:true,
+    imageUrl:true,
+    authorImage:true,
+    authorName:true,
+    id:true,
+    createdAt:true
+  }
+ });
+ return data;
+}
+export default async function Home() {
+  const data=await getData();
   return (
     <div>
-      <p>Hello from app page</p>
+     <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Proj</h1>
+     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      {data.map((item)=>(
+        <h1 key={item.title}>{item.title}</h1>
+      ))}
+
+     </div>
     </div>
   );
 }
