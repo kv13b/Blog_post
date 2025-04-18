e66a14b7b122684c9a0e6e0363e6d43f56581b94
karthik <karthikbhatt22@gmail.com>
2025-04-18 06:29:21 +0530

    kinde authentication

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
-export default function DashBoard(){
-    return(
-        <div>
-            <h1>dashboard</h1>
-        </div>
-    )
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashBoard() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    return redirect("/api/auth/register");
+  }
+  return (
+    <div>
+      <h1>dashboard</h1>
+    </div>
+  );
 }
--- my-app/components/general/NavBar.tsx
+++ my-app/components/general/NavBar.tsx
@@ -1,30 +1,56 @@
 import Link from "next/link";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function NavBar(){
-    return(
-        <nav className="py-5 flex items-center justify-between">
-            <div className="flex items-center gap-6">
-            <Link href="/">
-                <h1 className="text-3xl font-semibold">
-                Blog<span className="text-blue-500">Karthik</span>
-            </h1>
-            </Link>
-            <div className="hidden sm:flex items-center gap-6">
-                <Link className="text-sm font-medium hover:text-blue-500 transition-colors" href="/">
-                Home
-                </Link>
-            </div>
-            <div className="hidden sm:flex items-center gap-6">
-                <Link className="text-sm font-medium hover:text-blue-500 transition-colors" href="/dashboard">
-                DashBoard
-                </Link>
-            </div>
-            </div>
-            <div className="flex items-center gap-4">
-                <Button>Login</Button>
-                <Button variant="secondary">SignUp</Button>
-            </div>
-        </nav>
-    )
+export async function NavBar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <h1 className="text-3xl font-semibold">
+            Blog<span className="text-blue-500">Karthik</span>
+          </h1>
+        </Link>
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/"
+          >
+            Home
+          </Link>
+        </div>
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/dashboard"
+          >
+            DashBoard
+          </Link>
+        </div>
+      </div>
+
+      {user ? (
+        <div className="flex items-center gap-4">
+          <p>{user.given_name}</p>
+          <LogoutLink className={buttonVariants({ variant: "secondary" })}>
+            LogOut
+          </LogoutLink>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <LoginLink className={buttonVariants()}>Login</LoginLink>
+          <RegisterLink className={buttonVariants({ variant: "secondary" })}>
+            SignUp
+          </RegisterLink>
+        </div>
+      )}
+    </nav>
+  );
 }
