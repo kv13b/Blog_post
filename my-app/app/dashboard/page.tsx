1695c22042606367eaa43c40692e17f5fa76d76f
karthik <karthikbhatt22@gmail.com>
2025-04-20 14:29:46 +0530

    blog post card

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,7 +1,24 @@
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { prisma } from "../utils/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+async function getData(UserId: String) {
+  const data = await prisma.blogPost.findMany({
+    where: {
+      authorId: UserId as string,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return data;
+}
 
 export default async function DashBoardRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user?.id as string);
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
@@ -9,6 +26,13 @@ export default async function DashBoardRoute() {
         <Link className={buttonVariants()} href="/dashboard/create">
           Create Blog
         </Link>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ my-app/components/general/BlogPostCard.tsx
@@ -0,0 +1,6 @@
+export function BlogPostCard() {
+  <div
+    className="group relative overflow-hidden rounded-lg border border-gray-200 bg-white
+  shadow-md transition-all hover:shadow-lg"
+  ></div>;
+}
