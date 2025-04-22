1bbc9c00fd5d256be40fb3cbe4f47090ac7227b2
karthik <karthikbhatt22@gmail.com>
2025-04-22 21:15:49 +0530

    skeleton in loading

--- my-app/actions.ts
+++ my-app/actions.ts
@@ -3,15 +3,20 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { prisma } from "./app/utils/db";
 import { redirect } from "next/navigation";
+import { use } from "react";
 
 export async function handleSubmission(formdata: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  if (!user) {
+    return redirect("/api/auth/register");
+  }
   const title = formdata.get("title");
   const content = formdata.get("content");
   const url = formdata.get("url");
   console.log(user);
-  const data = await prisma.blogPost.create({
+
+  await prisma.blogPost.create({
     data: {
       title: title as string,
       content: content as string,
